7efd6f424405c8d5ada29b9fd4b54fc10a8e28db
Iago William Bastos de Souza <iagowilliam@hotmail.com>
2023-10-12 21:49:30 -0300

    Realizada a conexão com o banco de dados

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,24 @@
 const mongoose = require("mongoose")
 
 // conexão
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+const conn = async() => {
+    
+    try {
+        
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.9ugqjvj.mongodb.net/`)
+
+        console.log("Conectou ao banco!")
+
+        return dbConn
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+conn()
+
+module.exports = conn
