5e74e6262d82c8fbfb95799e71d74b8d7f13125f
Iago William Bastos de Souza <iagowilliam@hotmail.com>
2023-10-27 01:09:48 -0300

    Feito o registro de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -13,8 +13,34 @@ const generateToken = (id) => {
 }
 
 // Registrando usuário e logando
-const register = async(req, res) => {
-    res.send("Registro")
+const register = async (req, res) => {
+    const { name, email, password } = req.body
+
+    // checar se o usuário existe
+    const user = await User.findOne({ email })
+
+    if (user) {
+        res.status(422).json({ errors: ["Por favor, utilize outro e-mail."] })
+    }
+
+    // Gerar hash de senha
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Criando usuário
+    const newUser = await User.create({ name, email, password: passwordHash })
+
+    // Se o usuário foi criado com sucesso, retorna o token
+    if (!newUser) {
+        res.status(422).json({ errors: ["Houve um erro, por favor tente mais tarde."] })
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
+
 }
 
 module.exports = {
